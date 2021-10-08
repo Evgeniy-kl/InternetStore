0eb5ac1176bc4335d114de6ce82fb153c3efbf70
Evgeniy-kl <70905217+Evgeniy-kl@users.noreply.github.com>
2021-10-08 14:45:22 +0300

    Update tests.py

--- store/Storeapp/tests.py
+++ store/Storeapp/tests.py
@@ -1,9 +1,50 @@
-from django.test.client import Client
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, Client, RequestFactory
+from .models import *
+from decimal import Decimal
+User = get_user_model()
+from .views import recalc_cart, AddToCartView
+user = get_user_model()
 
-c = Client()
-response = c.post('/login/', {'username': 'admin', 'password': 123})
-response2 = c.post('/registration/', {'username': 'student2213', 'password': 123, 'confirm_password': 123, 'email': 'z_klimovich@bk.ru'})
-print(response.status_code)
-print(response.content)
-print(response2.content)
-print(response2.status_code)
+
+class InternetStoreCases(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(username='testuser', password='password')
+        self.category = Category.objects.create(name='Ноутбуки', slug='notebooks')
+        image = SimpleUploadedFile("mb_m1.png", content=b'', content_type='image.png')
+        self.notebook = Product.objects.create(
+            category=self.category,
+            title='TEST NOTEBOOK',
+            slug='test-slug',
+            image=image,
+            price=Decimal('12.00'),
+            description='SOME TEST TEXT',
+        )
+        self.customer = Customer.objects.create(
+            user=self.user,
+            phone='111',
+            address='sdf',
+        )
+        self.cart = Cart.objects.create(owner=self.customer)
+        self.cart_product = CartProduct.objects.create(
+            user=self.customer,
+            cart=self.cart,
+            product=self.notebook,
+        )
+
+    def test_add_to_cart(self):
+        self.cart.products.add(self.cart_product)
+        recalc_cart(self.cart)
+        self.assertIn(self.cart_product, self.cart.products.all())
+        self.assertEqual(self.cart.products.count(), 1)
+        self.assertEqual(self.cart.final_price, Decimal('12.00'))
+
+    def test_response(self):
+        factory = RequestFactory()
+        client = Client()
+        request = factory.get('')
+        response = AddToCartView.as_view()(request, slug='test-slug')
+        #response = client.get('/add-to-cart/TEST NOTEBOOK/test-slug')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.url, 'base')
